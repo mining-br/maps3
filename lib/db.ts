9797c26cd9586e849113efc7fbba0c18e659d991
mining-br <e.antonietto@fronteraminerals.com>
2025-10-06 14:57:19 -0300

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,12 @@
-import Database from "better-sqlite3";
+import DatabaseCtor, { Database as BetterSqlite3Database } from "better-sqlite3";
 import path from "node:path";
 
-let db: Database.Database | null = null;
+let db: BetterSqlite3Database | null = null;
 
-export function getDB(){
+export function getDB() {
   if (!db) {
     const file = path.join(process.cwd(), "data", "atlas.db");
-    db = new Database(file, { fileMustExist: true });
+    db = new DatabaseCtor(file, { fileMustExist: true });
   }
   return db;
 }
