4329b7a6f94024e3d403cbabba53c79d5da991d4
mining-br <e.antonietto@fronteraminerals.com>
2025-10-06 15:35:02 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { typedRoutes: true }
-}
-export default nextConfig
+  experimental: {
+    typedRoutes: true,
+    // inclui a pasta data/ na função /api/search
+    outputFileTracingIncludes: {
+      "/api/search": ["data/**"],
+    },
+  },
+};
+export default nextConfig;
