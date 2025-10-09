a35df3daecdae67b263a46d97347d5e3284c3e57
mining-br <e.antonietto@fronteraminerals.com>
2025-10-09 09:28:02 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    typedRoutes: true,
-    // Inclui arquivos da pasta data/ nas funções (necessário pra Vercel achar vercel_data.json)
-    outputFileTracingIncludes: {
-      "/api/**": ["data/**"],
-    },
-  },
+  reactStrictMode: true
 };
 
 export default nextConfig;
