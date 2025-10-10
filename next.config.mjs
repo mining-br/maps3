0a5ed8d583b8395ad0956133b669ebccd11baa36
mining-br <e.antonietto@fronteraminerals.com>
2025-10-10 10:16:01 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
+// next.config.mjs
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  webpack: (config) => {
+    // Alias @ -> raiz do projeto
+    config.resolve.alias["@"] = __dirname;
+    return config;
+  },
 };
 
 export default nextConfig;
